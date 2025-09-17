e07662b63fd3c4fe3daa6b4ac362e7c123a63cf1
krishchapla <krishc.gwis@gmail.com>
2025-09-17 09:46:49 +0530

    initial commit

--- app/src/main/java/com/example/mad_23012531080_practical3/LoginActivity.kt
+++ app/src/main/java/com/example/mad_23012531080_practical3/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mad_23012531080_practical3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        findViewById<Button>(R.id.login).setOnClickListener {
+            intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
--- app/src/main/java/com/example/mad_23012531080_practical3/MainActivity.kt
+++ app/src/main/java/com/example/mad_23012531080_practical3/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.mad_23012531080_practical3
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +20,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        findViewById<Button>(R.id.button6).setOnClickListener {
+            val loginintent = Intent(this, LoginActivity::class.java)
+            startActivity(loginintent)
+            }
+
+
     }
 }
 
