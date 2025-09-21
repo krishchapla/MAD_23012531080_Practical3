3c9ef366d6cb42b0bcb2b50aa5fa15e05a39f8b8
krishchapla <krishc.gwis@gmail.com>
2025-09-21 18:01:07 +0530

    initial commit

--- app/src/main/java/com/example/mad_23012531080_practical3/MainActivity.kt
+++ app/src/main/java/com/example/mad_23012531080_practical3/MainActivity.kt
@@ -4,13 +4,18 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+    private lateinit var editurl: EditText
+    private lateinit var phone: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,6 +29,20 @@ class MainActivity : AppCompatActivity() {
             val loginintent = Intent(this, LoginActivity::class.java)
             startActivity(loginintent)
             }
+
+        findViewById<Button>(R.id.button7).setOnClickListener {
+            val url = editurl.text.toString()
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = android.net.Uri.parse(url)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val phone = phone.text.toString()
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = android.net.Uri.parse("tel:$phone")
+            startActivity(intent)
+
+        }
 
 
     }
